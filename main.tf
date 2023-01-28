03f9186e69d95e5b82ec412216119f6be7e2b7eb
smartramana <smartramana@gmail.com>
2023-01-28 18:37:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "aws_eip" "nat" {
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public.id
-  depends_on    = [aws_internet_gateway.main_gateway]
+#   depends_on    = [aws_internet_gateway.main_gateway]
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
