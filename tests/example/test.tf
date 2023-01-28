e9303c8142ecf367c327b541a9a81b8661c8c8d2
smartramana <smartramana@gmail.com>
2023-01-28 18:17:40 -0500

    Update test.tf

--- tests/example/test.tf
+++ tests/example/test.tf
@@ -18,7 +18,7 @@ module "main" {
   # the main module directory.
   source = "../.."
 
-  aws_region          = "us-east-2"
+  aws_region          = "us-east"
   main_vpc_cidr       = "10.10.0.0/16"
   private_subnet_cidr = "10.10.1.0/24"
   public_subnet_cidr  = "10.10.2.0/24"
