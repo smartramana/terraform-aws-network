2a973c6d6184dfb61f667733cc6625d1e709b59d
smartramana <smartramana@gmail.com>
2023-01-28 18:21:53 -0500

    Update terraform_aws_network_example_test.go

--- tests/terraform_aws_network_example_test.go
+++ tests/terraform_aws_network_example_test.go
@@ -22,7 +22,7 @@ func TestTerraformAwsNetworkExample(t *testing.T) {
 			"main_vpc_cidr":       "10.10.0.0/16",
 			"private_subnet_cidr": "10.10.1.0/24",
 			"public_subnet_cidr":  "10.10.2.0/24",
-			"aws_region":          "us-east-2",
+			"aws_region":          "us-east",
 			"tag_name":            "demo",
 		},
 	})
